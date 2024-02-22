c71484c54df430ca73664628aead98c1a4753618
santosh2555 <santosh2555@gmail.com>
2024-02-22 16:41:12 -0500

    first commit

---
+++ src/main/java/com/santosh/PoghMSApplication.java
@@ -0,0 +1,13 @@
+package com.santosh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoghMSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoghMSApplication.class, args);
+	}
+
+}
